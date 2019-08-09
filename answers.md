3c11c3be7ccf0a971aae7fed59e69b6afee9a020
Blake Goms <blakegoms@gmail.com>
2019-08-09 11:19:46 -0700

    MVP: added comments for clarity and answers file

---
+++ answers.md
@@ -0,0 +1,20 @@
+ Mention two parts of Express that you learned about this week.
+
+express is a middleware and express is the standard server framework for node js
+
+
+ What is Express Middleware?
+
+ express sits on top of node js and makes it easier to create web apps and lets us serve SPA 
+
+ How can a client send data to an API?
+
+ a client can send data to an api through JSON 
+
+ What can the API return to help clients know if a request was successful?
+
+ an api can return a status code that follows the standard status codes maintained  by the IANA, Internet Assigned Numbers Authority
+
+ What Express feature can we use to partition an application into sub-applications?
+
+ We can use express to help with Routing
--- chores/choresRouter.js
+++ chores/choresRouter.js
@@ -1,45 +1,5 @@
 const router = require('express').Router()
 
-// let people = [
-//     {
-//         id: 1,
-//         name: 'Frodo Baggins',
-//     },
-//     {
-//         id: 2,
-//         name: 'Samewise',
-//     },
-//     {
-//         id: 3,
-//         name: 'Aragorn',
-//     }    
-// ]
-
-// let chores = [
-//     {
-//         id: 1,
-//         description: 'take the ring to Mordor',
-//         notes: 'make your way to Mount Doom',
-//         assignedTo: 1, // the id of Frodo,
-//         completed: true
-//     },
-//     {
-//         id: 2,
-//         description: 'destroy the ring',
-//         notes: 'cast the ring into the fire inside Mount Doom',
-//         assignedTo: 2,
-//         completed: false
-//     },
-//     {
-//         id: 3,
-//         description: 'Become who I was born to be',
-//         notes: 'For the men of the west!',
-//         assignedTo: 3,
-//         completed: true
-//     },
-// ]
-
-
 const people = [
     {
         id: 1,
@@ -109,83 +69,84 @@ const people = [
 
 ]
 
-
+//This will get each person
 router.get('/', (req,res)=>{
     res.status(200).json(people);
 })
 
+//this grabs the id of the person and their chores
 router.get('/:id', (req,res)=>{
     const id = req.params.id;
-    let [currentPerson] = people.filter( person => person.id == id);
-    if(currentPerson){
-        res.status(200).json(currentPerson);
+    let [Person] = people.filter( person => person.id == id);
+    if(Person){
+        res.status(200).json(Person);
     } else {
         res.status(400).json({message: "Oh no! Couldn't find that user!"})
     }
 })
 
-
+// this grabs the id of a person but only shows their chores
 router.get('/:id/chores', (req,res)=>{
     const id = req.params.id;
     const completed = req.query.completed
 
-    let [currentPerson] = people.filter( person => person.id == id);
+    let [Person] = people.filter( person => person.id == id);
 
-    if(currentPerson){
+    if(Person){
         if(completed == "true"){
-            let completedChores = currentPerson.chores.filter(chore => chore.completed === true)
+            let completedChores = Person.chores.filter(chore => chore.completed === true)
 
             res.status(200).json(completedChores)
         } else {
-            res.status(200).json(currentPerson.chores)
+            res.status(200).json(Person.chores)
         }
     }  else {
         res.status(400).json({message: "Oh no! Couldn't find that user!"})
     }
 })
 
-
+//this lets us post chores to a person
 router.post('/:id/chores', (req,res)=>{
     const id = req.params.id;
     const body = req.body;
-    let [currentPerson] = people.filter( person => person.id == id);
+    let [Person] = people.filter( person => person.id == id);
     if(!body.assignedTo) {
         res.status(404).json({message: "You need an assignedTo key!"})
     }
-    else if(currentPerson){
-        currentPerson.chores.push(body);
-        res.status(200).json(currentPerson.chores)
+    else if(Person){
+        Person.chores.push(body);
+        res.status(200).json(Person.chores)
     }  else {
         res.status(400).json({message: "Oh no! Couldn't find that user!"})
     }
 })
 
-
+// this lets us update the chores for a user
 router.put('/:id/chores', (req,res)=>{
     const id = req.params.id;
     const body = req.body;
-    let [currentPerson] = people.filter( person => person.id == id);
+    let [Person] = people.filter( person => person.id == id);
     if(!body.assignedTo && !body.id) {
         res.status(404).json({message: "You need an assignedTo key and an ID!!"})
     }
-    else if(currentPerson){
-        let newChores = currentPerson.chores.filter(chore => chore.id != body.id);
-        currentPerson.chores = [...newChores, body]
-        res.status(200).json(currentPerson.chores)
+    else if(Person){
+        let newChores = Person.chores.filter(chore => chore.id != body.id);
+        Person.chores = [...newChores, body]
+        res.status(200).json(Person.chores)
     }  else {
         res.status(400).json({message: "Oh no! Couldn't find that user!"})
     }
 })
 
-
+//this lets us delete all chores from a person
 router.delete('/:id/chores/:choreId', (req,res)=>{
     const userId = req.params.id;
     const choreId = req.params.choreId
-    let [currentPerson] = people.filter( person => person.id == userId);
-    let newChores = currentPerson.chores.filter(chore => chore.id != choreId)
-    if(currentPerson){
-        currentPerson.chores = [...newChores]
-        res.status(200).json(currentPerson.chores)
+    let [Person] = people.filter( person => person.id == userId);
+    let newChores = Person.chores.filter(chore => chore.id != choreId)
+    if(Person){
+        Person.chores = [...newChores]
+        res.status(200).json(Person.chores)
     }  else {
         res.status(400).json({message: "Oh no! Couldn't find that user!"})
     }
